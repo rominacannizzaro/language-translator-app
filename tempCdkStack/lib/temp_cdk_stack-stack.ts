3ceced611ee85ba87e82ba69ff39c2ad55083831
Romina Cannizzaro <rominacannizzaro@gmail.com>
2025-06-08 18:40:09 +0300

    feat: add API Gateway REST API to expose timeOfDay Lambda via GET

--- tempCdkStack/lib/temp_cdk_stack-stack.ts
+++ tempCdkStack/lib/temp_cdk_stack-stack.ts
@@ -2,15 +2,20 @@ import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as lambdaNodeJs from 'aws-cdk-lib/aws-lambda-nodejs'; 
+import * as apigateway from 'aws-cdk-lib/aws-apigateway';
 export class TempCdkStackStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // timeOfDay lambda construct
-    new lambdaNodeJs.NodejsFunction(this, "timeOfDay", {
+    const lambdaFunc = new lambdaNodeJs.NodejsFunction(this, "timeOfDay", {
       entry: "./lambda/timeOfDay.js",
       handler: "index",
       runtime: lambda.Runtime.NODEJS_20_X, 
     });
+
+    // create Rest Api
+    const restApi = new apigateway.RestApi(this, "timeOfDayRestAPI");
+    restApi.root.addMethod("GET", new apigateway.LambdaIntegration(lambdaFunc));
   }
 }
