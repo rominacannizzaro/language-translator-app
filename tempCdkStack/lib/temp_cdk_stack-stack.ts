d0c75e561e688d85d923fccf033cc9ddcbd02b3d
Romina Cannizzaro <rominacannizzaro@gmail.com>
2025-06-09 16:28:58 +0300

    feat: create a policy to allow to access the Amazon Translate's TranslateText API and attach it to the lambda

--- tempCdkStack/lib/temp_cdk_stack-stack.ts
+++ tempCdkStack/lib/temp_cdk_stack-stack.ts
@@ -3,15 +3,25 @@ import { Construct } from 'constructs';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as lambdaNodeJs from 'aws-cdk-lib/aws-lambda-nodejs'; 
 import * as apigateway from 'aws-cdk-lib/aws-apigateway';
+import * as iam from 'aws-cdk-lib/aws-iam';
+
 export class TempCdkStackStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    // Create policy to allow to access the Amazon Translate's TranslateText API
+    // to attached to the lambda
+    const translateAccessPolicy = new iam.PolicyStatement({
+      actions: ["translate:TranslateText"],
+      resources: ["*"],
+    })
 
     // timeOfDay lambda construct
     const lambdaFunc = new lambdaNodeJs.NodejsFunction(this, "timeOfDay", {
       entry: "./lambda/timeOfDay.js",
       handler: "index",
-      runtime: lambda.Runtime.NODEJS_20_X, 
+      runtime: lambda.Runtime.NODEJS_20_X,
+      initialPolicy: [translateAccessPolicy],
     });
 
     // create Rest Api
