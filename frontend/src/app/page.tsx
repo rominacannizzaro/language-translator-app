30689371fb7ca2eec85c829fd6d87e9f53d4b997
Romina Cannizzaro <rominacannizzaro@gmail.com>
2025-06-10 18:16:52 +0300

    feat: add initial implementation to translate from frontend

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,103 +1,66 @@
-import Image from "next/image";
+'use client';
+
+import { useState } from "react";
+
+// API Gateway URL (add the actual URL below)
+const URL="https://your-api-id.execute-api.region.amazonaws.com/prod"
+
+// Function to make HTTP call to our server, make a translation request and receive it
+function translateText({
+  inputLang,
+  outputLang,
+  inputText
+} : { 
+  inputLang: string;
+  outputLang: string;
+  inputText: string;
+}) {
+  return fetch(URL, { 
+    method: "POST",
+    body: JSON.stringify(
+      {
+        sourceLang: inputLang,
+        targetLang: outputLang,
+        text: inputText
+      }
+    )
+  })
+  .then(result => result.json())
+  .catch(e => e.toString());
+}
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  const [ inputText, setInputText ] = useState<string>("")
+  const [ inputLang, setInputLang ] = useState<string>("")
+  const [ outputLang, setOutputLang ] = useState<string>("")
+  const [ outputText, setOutputText ] = useState<any>(null)
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <form onSubmit={async (event) => {
+        event.preventDefault();
+        // console.log({inputText, inputLang, outputLang});
+        const result = await translateText({inputText, inputLang, outputLang});
+        setOutputText(result);
+      }}>
+        <div>
+          <label htmlFor="inputText">Input text</label>
+          <textarea id="inputText" className="bg-white" value={inputText} onChange={event => setInputText(event.target.value)} />
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+
+        <div>
+          <label htmlFor="inputLang">Input language</label>
+          <textarea id="inputLang" className="bg-white" value={inputLang} onChange={event => setInputLang(event.target.value)} />
+        </div>
+
+        <div>
+          <label htmlFor="outputLang">Output language</label>
+          <textarea id="outputLang" className="bg-white" value={outputLang} onChange={event => setOutputLang(event.target.value)} />
+        </div>
+
+        <button className="btn bg-blue-500 p-2 mt-2 rounded-xl" type="submit">Translate</button>
+      </form>
+      <pre className="whitespace-pre-wrap w-full">{JSON.stringify(outputText)}</pre>
+    </main>
   );
 }
