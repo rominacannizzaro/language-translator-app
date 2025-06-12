3feeb7282d8463f3ed5cf2c390240c5036502480
Romina Cannizzaro <rominacannizzaro@gmail.com>
2025-06-12 08:38:09 +0300

    refactor: update outputText state to have type TranslateResponse or null

--- apps/frontend/src/app/page.tsx
+++ apps/frontend/src/app/page.tsx
@@ -40,7 +40,7 @@ export default function Home() {
   const [inputText, setInputText] = useState<string>("");
   const [inputLang, setInputLang] = useState<string>("");
   const [outputLang, setOutputLang] = useState<string>("");
-  const [outputText, setOutputText] = useState<any>(null);
+  const [outputText, setOutputText] = useState<TranslateResponse | null>(null);
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
