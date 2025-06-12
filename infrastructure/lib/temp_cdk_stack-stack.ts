7d535f5b7bf27359743b5ad9acf16dc57be4cd94
Romina Cannizzaro <rominacannizzaro@gmail.com>
2025-06-12 17:36:23 +0300

    feat: update in cdk the lambda entry path

--- infrastructure/lib/temp_cdk_stack-stack.ts
+++ infrastructure/lib/temp_cdk_stack-stack.ts
@@ -1,9 +1,10 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-import * as lambda from 'aws-cdk-lib/aws-lambda';
-import * as lambdaNodeJs from 'aws-cdk-lib/aws-lambda-nodejs'; 
-import * as apigateway from 'aws-cdk-lib/aws-apigateway';
-import * as iam from 'aws-cdk-lib/aws-iam';
+import * as cdk from "aws-cdk-lib";
+import * as path from "path";
+import { Construct } from "constructs";
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import * as lambdaNodeJs from "aws-cdk-lib/aws-lambda-nodejs";
+import * as apigateway from "aws-cdk-lib/aws-apigateway";
+import * as iam from "aws-cdk-lib/aws-iam";
 
 export class TempCdkStackStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -14,11 +15,22 @@ export class TempCdkStackStack extends cdk.Stack {
     const translateAccessPolicy = new iam.PolicyStatement({
       actions: ["translate:TranslateText"],
       resources: ["*"],
-    })
+    });
+
+    const monorepoRoot = path.join(__dirname, "../../");
+
+    const translateLambdaPath = path.join(
+      monorepoRoot,
+      "packages",
+      "lambdas",
+      "translate",
+      "index.ts"
+    );
 
     // timeOfDay lambda construct
     const lambdaFunc = new lambdaNodeJs.NodejsFunction(this, "timeOfDay", {
-      entry: "./lambda/timeOfDay.ts",
+      projectRoot: monorepoRoot,
+      entry: translateLambdaPath,
       handler: "index",
       runtime: lambda.Runtime.NODEJS_20_X,
       initialPolicy: [translateAccessPolicy],
@@ -26,6 +38,9 @@ export class TempCdkStackStack extends cdk.Stack {
 
     // create Rest Api
     const restApi = new apigateway.RestApi(this, "timeOfDayRestAPI");
-    restApi.root.addMethod("POST", new apigateway.LambdaIntegration(lambdaFunc));
+    restApi.root.addMethod(
+      "POST",
+      new apigateway.LambdaIntegration(lambdaFunc)
+    );
   }
 }
