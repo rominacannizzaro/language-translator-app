c86d70f41f0c79fdb8a722951a6590b1c375fb98
Romina Cannizzaro <rominacannizzaro@gmail.com>
2025-06-12 17:38:54 +0300

    feat: add TranslateDbObject type

--- packages/shared-types/src/translate.d.ts
+++ packages/shared-types/src/translate.d.ts
@@ -1,12 +1,18 @@
 // Types for translation request and response
 
 export type TranslateRequest = {
-    sourceLang: string;
-    targetLang: string;
-    sourceText: string;
+  sourceLang: string;
+  targetLang: string;
+  sourceText: string;
 };
 
 export type TranslateResponse = {
-    timestamp: string;
-    targetText: string;
+  timestamp: string;
+  targetText: string;
 };
+
+// Type that represents the data to be stored in the database
+export type TranslateDbObject = TranslateRequest &
+  TranslateResponse & {
+    requestId: string; // value to be used to 1) store unique entries in the database and to 2) retrieve specific data
+  };
