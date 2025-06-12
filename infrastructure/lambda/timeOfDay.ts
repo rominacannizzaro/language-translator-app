3f478b52bd51b047102ba217c23d8406971c2a4a
Romina Cannizzaro <rominacannizzaro@gmail.com>
2025-06-12 09:11:55 +0300

    feat: add logging and input validation in lambda

--- infrastructure/lambda/timeOfDay.ts
+++ infrastructure/lambda/timeOfDay.ts
@@ -1,62 +1,75 @@
 import * as clientTranslate from "@aws-sdk/client-translate";
 import * as lambda from "aws-lambda";
-import {TranslateRequest, TranslateResponse } from "@translator/shared-types"
+import { TranslateRequest, TranslateResponse } from "@translator/shared-types";
 
 // Initialize the AWS Translate client
 const translateClient = new clientTranslate.TranslateClient({});
 
 export const index: lambda.APIGatewayProxyHandler = async function (
-    event: lambda.APIGatewayProxyEvent
+  event: lambda.APIGatewayProxyEvent
 ) {
-    try {
-        if (!event.body){
-            throw new Error("body is empty.")
-        };
-        const body = JSON.parse(event.body) as TranslateRequest; // the translate object that comes in into the lambda
-        const { sourceLang, targetLang, sourceText } = body;
+  try {
+    if (!event.body) {
+      throw new Error("body is missing.");
+    }
+    console.log(event.body);
 
-        // Get current time in human-readable format
-        const now = new Date(Date.now()).toString();
+    const body = JSON.parse(event.body) as TranslateRequest; // the translate object that comes in into the lambda
 
-        // Create translation command
-        const translateCmd = new clientTranslate.TranslateTextCommand({
-            SourceLanguageCode: sourceLang,
-            TargetLanguageCode: targetLang,
-            Text: sourceText,
-        });
+    if (!body.sourceLang) {
+      throw new Error("sourceLang is missing");
+    }
+    if (!body.targetLang) {
+      throw new Error("targetLang is missing");
+    }
+    if (!body.sourceText) {
+      throw new Error("sourceText is missing");
+    }
 
-        // Send the command and wait for response
-        const result = await translateClient.send(translateCmd);
+    const { sourceLang, targetLang, sourceText } = body;
 
-        if(!result.TranslatedText) {
-            throw new Error("translation is empty");
-        };
+    // Get current time in human-readable format
+    const now = new Date(Date.now()).toString();
 
-        const rtnData: TranslateResponse = {
-            timestamp: now,
-            targetText: result.TranslatedText,
-        }
+    // Create translation command
+    const translateCmd = new clientTranslate.TranslateTextCommand({
+      SourceLanguageCode: sourceLang,
+      TargetLanguageCode: targetLang,
+      Text: sourceText,
+    });
 
-        return {
-            statusCode: 200,
-            headers: {
-                "Access-Control-Allow-Origin": "*", // allows requests from any origin
-                "Access-Control-Allow-Credentials": true, // required for cookies, authorization headers, etc.
-                "Access-Control-Allow-Headers": "*", // allows all standard and custom headers
-                "Access-Control-Allow-Methods": "*", // allows all standard and custom headers
-            },
-            body: JSON.stringify(rtnData),
-        };
-    } catch (e: any) {
-        return {
-            statusCode: 500,
-            headers: {
-                "Access-Control-Allow-Origin": "*",
-                "Access-Control-Allow-Credentials": true,
-                "Access-Control-Allow-Headers": "*",
-                "Access-Control-Allow-Methods": "*",
-            },
-            body: JSON.stringify(e.toString()),
-        }
+    // Send the command and wait for response
+    const result = await translateClient.send(translateCmd);
+
+    if (!result.TranslatedText) {
+      throw new Error("translation is empty");
+    }
+
+    const rtnData: TranslateResponse = {
+      timestamp: now,
+      targetText: result.TranslatedText,
     };
+
+    return {
+      statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "*", // allows requests from any origin
+        "Access-Control-Allow-Credentials": true, // required for cookies, authorization headers, etc.
+        "Access-Control-Allow-Headers": "*", // allows all standard and custom headers
+        "Access-Control-Allow-Methods": "*", // allows all standard and custom headers
+      },
+      body: JSON.stringify(rtnData),
+    };
+  } catch (e: any) {
+    return {
+      statusCode: 500,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Credentials": true,
+        "Access-Control-Allow-Headers": "*",
+        "Access-Control-Allow-Methods": "*",
+      },
+      body: JSON.stringify(e.toString()),
+    };
+  }
 };
