387cd153de24d14f032ec354669ca6c6b75f241a
Romina Cannizzaro <rominacannizzaro@gmail.com>
2025-06-13 11:00:33 +0300

    feat: add API gateway response helpers into utils-lambda-layer dir

---
+++ packages/lambda-layers/utils-lambda-layer/src/gatewayResponse.ts
@@ -0,0 +1,34 @@
+import * as lambda from "aws-lambda";
+
+const createGatewayResponse = ({
+  statusCode,
+  body,
+}: {
+  statusCode: number;
+  body: string;
+}): lambda.APIGatewayProxyResult => {
+  return {
+    statusCode,
+    headers: {
+      "Access-Control-Allow-Origin": "*", // allows requests from any origin
+      "Access-Control-Allow-Credentials": true, // required for cookies, authorization headers, etc.
+      "Access-Control-Allow-Headers": "*", // allows all standard and custom headers
+      "Access-Control-Allow-Methods": "*", // allows all standard and custom headers
+    },
+    body,
+  };
+};
+
+export const createSuccessJsonResponse = (body: object) => {
+  createGatewayResponse({
+    statusCode: 200,
+    body: JSON.stringify(body),
+  });
+};
+
+export const createErrorJsonResponse = (body: object) => {
+  createGatewayResponse({
+    statusCode: 500,
+    body: JSON.stringify(body),
+  });
+};
