32cd302aaea2ea5bbf3339a17cb7177499442348
Romina Cannizzaro <rominacannizzaro@gmail.com>
2025-06-15 09:23:52 +0300

    feat: configure Next.js for making static export and define custom dist directory

--- apps/frontend/next.config.ts
+++ apps/frontend/next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // Configure Next.js to build the website  as a static site.
+  // This is useful for hosting on static file servers or CDNs like GitHub Pages, S3, or Netlify.
+  output: "export",
+  // Changes the default build output directory from '.next' to 'dist'.
+  // This is helpful for monorepos or deployments where "dist" is a convention.
+  distDir: "dist",
 };
 
 export default nextConfig;
