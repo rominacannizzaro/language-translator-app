d11172e2ce5370bf5fe59c31465949643e3c4848
Romina Cannizzaro <rominacannizzaro@gmail.com>
2025-06-16 23:05:32 +0300

    feat: configure CDK stack with explicit AWS account and region

--- infrastructure/bin/temp_cdk_stack.ts
+++ infrastructure/bin/temp_cdk_stack.ts
@@ -1,6 +1,12 @@
 #!/usr/bin/env node
-import * as cdk from 'aws-cdk-lib';
-import { TempCdkStackStack } from '../lib/temp_cdk_stack-stack';
+import * as cdk from "aws-cdk-lib";
+import { TempCdkStackStack } from "../lib/temp_cdk_stack-stack";
 
 const app = new cdk.App();
-new TempCdkStackStack(app, 'TempCdkStackStack', {});
+new TempCdkStackStack(app, "TempCdkStackStack", {
+  // Account and Region to deploy the stack to.
+  env: {
+    account: "yourAccountId",
+    region: "us-east-1", // Important: ACM certificates used w/CloudFront distributions must be created in the us-east-1 (N. Virginia) region
+  },
+});
