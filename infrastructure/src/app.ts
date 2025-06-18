eabc47a01895f07c09116c8ce175824d76b0e301
Romina Cannizzaro <rominacannizzaro@gmail.com>
2025-06-18 18:10:57 +0300

    refactor: update CDK app using environment variables

--- infrastructure/src/app.ts
+++ infrastructure/src/app.ts
@@ -1,12 +1,15 @@
 #!/usr/bin/env node
 import * as cdk from "aws-cdk-lib";
 import { TranslatorServiceStack } from "./stacks";
+import { getConfig } from "./helpers";
+
+const config = getConfig();
 
 const app = new cdk.App();
 new TranslatorServiceStack(app, "TranslatorService", {
   // Account and Region to deploy the stack to.
   env: {
-    account: "yourAccountId",
-    region: "us-east-1", // Important: ACM certificates used w/CloudFront distributions must be created in the us-east-1 (N. Virginia) region
+    account: config.awsAccountId,
+    region: config.awsRegion, // Important: ACM certificates used w/CloudFront distributions must be created in the us-east-1 (N. Virginia) region
   },
 });
