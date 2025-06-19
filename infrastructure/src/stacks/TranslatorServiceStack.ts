dde3b326ddb887591af6e0721ade0178813ebc27
Romina Cannizzaro <rominacannizzaro@gmail.com>
2025-06-19 19:44:34 +0300

    feat: add UserAuthSupportService to TranslatorServiceStack to provision user authentication

--- infrastructure/src/stacks/TranslatorServiceStack.ts
+++ infrastructure/src/stacks/TranslatorServiceStack.ts
@@ -5,6 +5,7 @@ import {
   TranslationService,
   StaticWebsiteDeployment,
   CertificateWrapper,
+  UserAuthSupportService,
 } from "../constructs";
 import { getConfig } from "../helpers";
 
@@ -23,6 +24,9 @@ export class TranslatorServiceStack extends cdk.Stack {
       apiUrl,
       webUrl,
     });
+
+    // User Auth support
+    const userAuth = new UserAuthSupportService(this, "userAuthSupport");
 
     const restApi = new RestApiService(this, "restApiService", {
       apiUrl,
