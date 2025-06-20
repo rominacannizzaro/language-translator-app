81e59c80383f39ee3c2497b739707529b3036c5b
Romina Cannizzaro <rominacannizzaro@gmail.com>
2025-06-20 11:09:50 +0300

    feat: implement user registration with AWS Amplify signUp at /register page

--- apps/frontend/src/app/register/page.tsx
+++ apps/frontend/src/app/register/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState } from "react";
-import { signIn, signOut } from "aws-amplify/auth";
+import { signUp, confirmSignUp } from "aws-amplify/auth";
 import Link from "next/link";
 
 export default function Register() {
@@ -10,7 +10,37 @@ export default function Register() {
   const [password2, setPassword2] = useState<string>("");
 
   return (
-    <form className="flex flex-col space-y-4" onSubmit={async (event) => {}}>
+    <form
+      className="flex flex-col space-y-4"
+      onSubmit={async (event) => {
+        event.preventDefault();
+        try {
+          if (password !== password2) {
+            throw new Error("Passwords don't match");
+          }
+
+          // Initiate the user sign-up process with AWS Amplify
+          const { nextStep } = await signUp({
+            username: email,
+            password: password,
+            options: {
+              userAttributes: {
+                email, // Pass email as a user attribute for Cognito
+              },
+              autoSignIn: true, // After successful sign-up, automatically attempt to sign the user in
+            },
+          });
+          /**
+           * When the signUp fc is called, it returns a 'nextStep' object, whose 'signUpStep' property indicates the current state of the sign-up flow.
+           * SignUpStep can be: "DONE" | "CONFIRM_SIGN_UP" | "COMPLETE_AUTO_SIGN_IN"
+           * When the user submits email and password twice on the /register page, it will return "CONFIRM_SIGN_UP":
+           * a confirmation email with a verification code has been sent to the user
+           * and user has been added to the Cognito User Pool with email_verified = false and confirmation status = "unconfirmed"
+           */
+          console.log("signUpStep:", nextStep.signUpStep);
+        } catch (e) {}
+      }}
+    >
       <div>
         <label htmlFor="email">Email:</label>
         <input
