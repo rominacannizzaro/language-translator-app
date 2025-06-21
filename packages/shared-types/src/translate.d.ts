b9f4fa38828fe9ab3ec0055f85d51012ca8d01d1
Romina Cannizzaro <rominacannizzaro@gmail.com>
2025-06-21 21:01:40 +0300

    feat: add username to TranslateDbObject type to store user's username w/each translation record in database

--- packages/shared-types/src/translate.d.ts
+++ packages/shared-types/src/translate.d.ts
@@ -14,5 +14,6 @@ export type TranslateResponse = {
 // Type that represents the data to be stored in the database
 export type TranslateDbObject = TranslateRequest &
   TranslateResponse & {
-    requestId: string; // value to be used to 1) store unique entries in the database and to 2) retrieve specific data
+    username: string;
+    requestId: string;
   };
