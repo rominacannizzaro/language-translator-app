0030da668f156e83d9d3cc1399cbb2b6a13e0a09
Romina Cannizzaro <rominacannizzaro@gmail.com>
2025-06-22 22:36:10 +0300

    feat: add Provider component to enable React Query state management across the app

---
+++ apps/frontend/src/components/Provider.tsx
@@ -0,0 +1,10 @@
+import React, { useState } from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+export function Provider({ children }: { children: React.ReactNode }) {
+  const [queryClient] = useState(() => new QueryClient());
+
+  return (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  );
+}
