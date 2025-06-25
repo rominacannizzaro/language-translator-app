877d0cce5db232deadc7651040c52af5429610c1
Romina Cannizzaro <rominacannizzaro@gmail.com>
2025-06-25 16:14:39 +0300

    feat: enhance register layout with centered content and styled container

--- apps/frontend/src/app/register/layout.tsx
+++ apps/frontend/src/app/register/layout.tsx
@@ -3,5 +3,9 @@ export default function Layout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return <main className="flex flex-col m-8">{children}</main>;
+  return (
+    <main className="flex flex-col justify-center items-center m-8">
+      <div className="bg-white p-4 shadow-lg rounded-lg w-80">{children}</div>
+    </main>
+  );
 }
